8230b1e5b20ab12c4f8b4f6368ac40778b7a3577
Jane Zanchetta <Jane Zanchetta@LAPTOP-3CU680IS>
2017-04-03 20:30:21 -0300

    First Commit!

---
+++ JShare140649/src/br/univel/jshare/comum/IServer.java
@@ -0,0 +1,19 @@
+package br.univel.jshare.comum;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.Map;
+
+public interface IServer extends Remote {
+	public static final String NOME_SERVICO = "JShare";
+	public void registrarCliente(Cliente c) throws RemoteException;
+	public void publicarListaArquivos(Cliente c, List<Arquivo> lista)
+			throws RemoteException;
+	public Map<Cliente, List<Arquivo>> procurarArquivo(String query, TipoFiltro tipoFiltro, String filtro)
+			throws RemoteException;
+	public byte[] baixarArquivo(Cliente cli, Arquivo arq) throws RemoteException;
+	public void desconectar(Cliente c) throws RemoteException;
+	
+
+}
