fbd1f14a754f74be32c15a4acceccea40fea1319
Jane Zanchetta <Jane Zanchetta@LAPTOP-3CU680IS>
2017-04-04 20:14:47 -0300

    Implementação do Servidor.

---
+++ JShare140649/src/br/univel/jshare/cliente/TelaCliente.java
@@ -0,0 +1,14 @@
+package br.univel.jshare.cliente;
+
+import javax.swing.JPanel;
+
+public class TelaCliente extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public TelaCliente() {
+
+	}
+
+}
